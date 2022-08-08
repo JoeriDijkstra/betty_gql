76ea3bd0e67d8b09c3dde4232e2cce3e42f53f2c
Joeri Dijkstra <joeri.dijkstra@bettyblocks.com>
2022-08-08 19:05:31 +0100

    Initial commit

---
+++ lib/betty_gql.ex
@@ -0,0 +1,19 @@
+defmodule BettyGql do
+
+  alias BettyGql.Functions.LoginFunctions
+  alias BettyGql.Queries.LoginQueries
+
+  def initialize do
+    {app_url, credentials} = LoginFunctions.get_login_variables()
+    Neuron.Config.set(url: app_url)
+
+    case Neuron.query(LoginQueries.login_query(credentials)) do
+      {:ok, resp} -> {:ok, LoginFunctions.extract_jwt_token(resp)}
+      {:error, message} -> {:error, message}
+    end
+  end
+
+  def make_request(jwt, query) do
+
+  end
+end
